3e403e22e5a4866f8e7c1b417a8664dccf8a81b3
Canisback <canisback@gmail.com>
2018-04-23 14:51:01 +0200

    add __init__ files

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup, find_packages
 setup(
   name = 'pantheon',
-  packages = find_packages(), # this must be the same as the name above
+  packages = find_packages(),
   version = '1.0.0',
   description = 'Riot API library for Python and asyncio',
   author = 'Canisback',
   author_email = 'canisback@gmail.com',
-  url = 'https://github.com/Canisback/pantheon', # use the URL to the github repo
-  download_url = 'https://github.com/Canisback/pantheon/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['Riot API', 'python'], # arbitrary keywords
+  url = 'https://github.com/Canisback/pantheon',
+  keywords = ['Riot API', 'python'],
   classifiers = [],
 )
